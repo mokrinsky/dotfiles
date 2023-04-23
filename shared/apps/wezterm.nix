7b68b18ea745b436e746a3accded51fd05f3290b
Nikolay Mokrinsky <me@mokrinsky.ru>
2023-04-23 23:43:15 +0400

    feat(wezterm): changes in wezterm config

--- shared/apps/wezterm.nix
+++ shared/apps/wezterm.nix
@@ -17,6 +17,7 @@
       return {
         adjust_window_size_when_changing_font_size = false,
         allow_square_glyphs_to_overflow_width = 'Always',
+        animation_fps = 1,
         audible_bell = 'Disabled',
         automatically_reload_config = true,
         color_scheme = 'Catppuccin Mocha', -- or Macchiato, Frappe, Latte
@@ -29,13 +30,16 @@
         enable_scroll_bar = false,
         font = wezterm.font_with_fallback {
           {
-            family = 'Berkeley Mono',
-            harfbuzz_features = { 'calt=0', 'clig=1', 'liga=1' },
+            family = 'JetBrains Mono',
+            harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
           },
         },
         font_size = 12,
+        front_end = 'Software',
         hide_tab_bar_if_only_one_tab = true,
         hyperlink_rules = hyperlink,
+        line_height = 0.9,
+        -- macos_window_background_blur = 20,
         scrollback_lines = 10000,
         swallow_mouse_click_on_window_focus = false,
         tab_bar_style = {
