e0352b4586505500450183d098e5dd3bba04fcea
Nikolay Mokrinsky <me@mokrinsky.ru>
2023-06-17 23:18:22 +0400

    feat(argolab): local storage for k3s

--- hosts/argolab/configuration.nix
+++ hosts/argolab/configuration.nix
@@ -66,7 +66,7 @@
   services = {
     k3s = {
       enable = true;
-      extraFlags = "--disable traefik --disable local-storage --disable metrics-server --flannel-backend=none --disable-network-policy";
+      extraFlags = "--disable traefik --disable metrics-server --flannel-backend=none --disable-network-policy";
     };
     openssh = {
       enable = true;
