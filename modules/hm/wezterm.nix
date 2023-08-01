924751cb0eab866fa0a457c48e4ff4d293f0de51
Nikolay Mokrinsky <me@mokrinsky.ru>
2023-08-01 16:48:19 +0400

    feat(wezterm): change wezterm config

--- modules/hm/wezterm.nix
+++ modules/hm/wezterm.nix
@@ -28,8 +28,6 @@ in {
         indicator = {
           leader = {
             enabled = false,
-            off = " ",
-            on = " ",
           },
           mode = {
             enabled = true,
@@ -50,13 +48,12 @@ in {
         },
         clock = { -- note that this overrides the whole set_right_status
           enabled = false,
-          format = "%H:%M", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
         },
       })
 
       local cs = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
-      cs.tab_bar.background = 'rgba(30, 30, 46, 0.8)'
-      cs.tab_bar.new_tab.bg_color = 'rgba(30, 30, 46, 0.8)'
+      cs.tab_bar.background = 'rgb(30, 30, 46)'
+      cs.tab_bar.new_tab.bg_color = 'rgb(30, 30, 46)'
 
       c.adjust_window_size_when_changing_font_size = false
       c.allow_square_glyphs_to_overflow_width = 'Always'
@@ -78,25 +75,7 @@ in {
       c.font_size = 13
       c.front_end = 'Software'
       c.hide_tab_bar_if_only_one_tab = true
-      c.hyperlink_rules = {
-        {
-          regex = [[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}]],
-          format = 'http://$0',
-        },
-        {
-          regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
-          format = '$0',
-        },
-        {
-          regex = [[\bfile://\S*\b]],
-          format = '$0',
-        },
-        {
-          regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-          format = '$0',
-        },
-      }
-      c.line_height = 0.9
+      c.line_height = 1.0
       c.macos_window_background_blur = 20
       c.scrollback_lines = 10000
       c.swallow_mouse_click_on_window_focus = false
@@ -107,13 +86,13 @@ in {
       c.tab_max_width = 32
       c.use_fancy_tab_bar = false
       c.use_ime = false
-      c.window_background_opacity = 0.8
+      c.window_background_opacity = 1.0
       c.window_decorations = 'RESIZE'
       c.window_padding = {
-        bottom = 0,
-        left = 5,
-        right = 5,
-        top = 0,
+        bottom = 35,
+        left = 35,
+        right = 35,
+        top = 25,
       }
 
       return c
